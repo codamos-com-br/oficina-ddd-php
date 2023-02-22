cc23b9e4ef48315a2042b254751898dbdb019818
Nawarian <nickolas@phpsp.org.br>
2023-02-22 10:30:42 +0100

    Adiciona teste de exemplo

---
+++ tests/SimpleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codamos\OficinaDddPhp\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testMyTest(): void
+    {
+        self::assertTrue(true);
+    }
+}
